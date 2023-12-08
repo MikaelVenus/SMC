d6d3ff9865e3ab6593610c7a54bf2665c29ae3a7
Sun <nantawatc99@gmail.com>
2023-12-08 07:58:27 +0700

    add Model 70%

--- smcproject/account/admin.py
+++ smcproject/account/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Menu
-from .models import Booking
-
+from .models import Menu, Booking, Customer, Item, Supplier, Location
 
 admin.site.register(Menu)
 admin.site.register(Booking)
+admin.site.register(Customer)
+admin.site.register(Item)
+admin.site.register(Supplier)
+admin.site.register(Location)
--- smcproject/account/models.py
+++ smcproject/account/models.py
@@ -1,5 +1,76 @@
 from django.db import models
 
+class Customer(models.Model):
+    customer_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return f"{self.customer_id} : {self.name} : {self.description[:20]}"
+    
+class Supplier(models.Model):
+    supplier_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return f"{self.supplier_id} : {self.name} : {self.description[:20]}"
+    
+class Location(models.Model):
+    location_id = models.IntegerField(primary_key=True)
+    location_type = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return f"{self.supplier_id} : {self.name} : {self.description[:20]}"
+    
+class Item(models.Model):
+    item_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=4,max_digits=8)
+    description = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return f"{self.item_id} : {self.name} : {self.description[:20]}"
+    
+class Order_Transection(models.Model):
+    
+    #Customer part
+    order_id = models.IntegerField(primary_key=True,max_length=8)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item,  on_delete=models.CASCADE)
+    target_location = models.ForeignKey(Location,  on_delete=models.CASCADE)
+    receive_phone = models.IntegerField(max_length=12)
+    delivery_date = models.DateField()
+    delivery_number = models.CharField(max_length=20)
+    receive_name = models.CharField(max_length=100)
+    q_sale = models.DecimalField(decimal_places=4,max_digits=8)
+    sale_price = models.DecimalField(decimal_places=4,max_digits=8)
+    toal_sale_amount = models.DecimalField(decimal_places=4,max_digits=8)
+    PAY_RECEIVE_METHOD_CHOICES = [
+        ('cash', 'Cash'),
+        ('qrscan', 'QR Scan'),
+        ('credit', 'Credit'),
+        ('check', 'Check'),
+    ]
+    # Define receive_method field with choices
+    receive_method = models.CharField(max_length=20, choices=PAY_RECEIVE_METHOD_CHOICES)
+    sale_vat = models.CharField(max_length=30)
+    pay_receive_date = models.DateField()
+    
+
+
+
+
+
+
+
+
+
+
+
 
 # Add code to create Menu model
 class Menu(models.Model):
